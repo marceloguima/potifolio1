f1abe80c042168bf5cd88b979d44264302e6c6a1
Marcelo guimaraes <marceloguimaraesdev@gmail.com>
2025-06-17 19:15:26 -0400

    resolvido bug do botão menu invertido

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const btnMostraMenu = document.querySelector(".fa-solid.fa-bars");
+const btnMostraMenu = document.querySelector(".fa-solid.fa-xmark");
 const menu = document.querySelector(".sidebar");
 
 btnMostraMenu.addEventListener("click", () => {
     menu.classList.toggle("active");
-    if(btnMostraMenu.classList.contains("fa-bars")){
-        btnMostraMenu.classList.remove("fa-bars")
-        btnMostraMenu.classList.add("fa-xmark")
-    }else{
+    if(btnMostraMenu.classList.contains("fa-xmark")){
         btnMostraMenu.classList.remove("fa-xmark")
         btnMostraMenu.classList.add("fa-bars")
+    }else{
+        btnMostraMenu.classList.remove("fa-bars")
+        btnMostraMenu.classList.add("fa-xmark")
     }
 });
 
