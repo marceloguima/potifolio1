162c85176f0bcc29c471c0094f00b4a4716513a7
Marcelo guimaraes <marceloguimaraesdev@gmail.com>
2025-06-17 22:02:46 -0400

    adicionei texto ao botão fechar

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const btnMostraMenu = document.querySelector(".fa-solid.fa-xmark");
 const menu = document.querySelector(".sidebar");
+const textoFechar = document.querySelector(".fechar")
 
 btnMostraMenu.addEventListener("click", () => {
     menu.classList.toggle("active");
     if(btnMostraMenu.classList.contains("fa-xmark")){
         btnMostraMenu.classList.remove("fa-xmark")
         btnMostraMenu.classList.add("fa-bars")
+        textoFechar.textContent = ""
     }else{
         btnMostraMenu.classList.remove("fa-bars")
         btnMostraMenu.classList.add("fa-xmark")
+                textoFechar.textContent = "Fechar"
+
     }
 });
 
